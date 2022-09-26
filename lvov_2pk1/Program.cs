48d2df8d012dbd9e92bb059062ce836f11fa3ab2
Маргарита Жуликова <m.zhulikova05@mail.ru>
2022-09-26 11:36:16 +0300

    Добавьте файлы проекта.

---
+++ lvov_2pk1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace lvov_2pk1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("задание №1:");           // задание №1
+            for (int i = -25; i <= 25; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("задание №2:");           // задание №2
+            char b = 'P';
+            char sym = '#';
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine(b);
+                b++;
+            }
+            Console.WriteLine("задание №4:");           // задание №4
+            for(int i =0; i <= 100; i+=1)
+            {
+                if(i № 2 == 0)
+               {
+                Console.WriteLine($"{i}");
+               }   
+            }
+
+
+        }                   
+            
+        
+    }
+}
---
+++ pz_6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace pz_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+}
