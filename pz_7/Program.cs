5bec850df3694c1f9faeeebee54cf77cc9c6a718
AlekseyLvov <91681465+AlekseiLvov@users.noreply.github.com>
2022-10-03 17:30:46 +0300

    Add files via upload
    добавил практическое задание №7

---
+++ pz_7/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace pz_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[8];
+            int[] massiv = Enumerable.Range(-10, 10).ToArray();
+            Random r = new Random();
+            for (int x = 0; x < myArray.Length; x++)
+            {
+                myArray[x] = r.Next(5);
+                Console.WriteLine(myArray[x]);
+            }
+        }
+    }
+}
