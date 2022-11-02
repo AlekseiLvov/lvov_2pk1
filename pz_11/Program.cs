5db06262453539c85264318a60975ffee4dbfbd3
AlekseyLvov <91681465+AlekseiLvov@users.noreply.github.com>
2022-11-02 19:01:31 +0300

    Add files via upload

---
+++ pz_11/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace pz_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+            string concat = String.Concat(x, y, z);
+            Console.WriteLine(concat);
+        }
+    }
+
+}
