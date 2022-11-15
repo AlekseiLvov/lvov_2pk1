289a04d1916df40e685256afff7a077254e952be
AlekseyLvov <91681465+AlekseiLvov@users.noreply.github.com>
2022-11-15 17:46:58 +0300

    Add files via upload

---
+++ pz_12/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace pz_12
+{
+    internal class Program
+    {
+        static void PowerA3(double A, out double B)
+        {
+            B = A * A * A;
+        }
+        static void Main(string[] args)
+        {
+            double b;
+            for (int i = 1; i < 6; i++)
+            {
+                PowerA3(i, out b);
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
