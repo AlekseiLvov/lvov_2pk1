71bb70b9c73746e2fe8b25e1e630352b0bd7cbad
AlekseyLvov <91681465+AlekseiLvov@users.noreply.github.com>
2022-11-24 15:40:00 +0300

    Add files via upload

---
+++ pz_15/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace pz_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = @"D:\test.txt";
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                string text = reader.ReadToEnd();
+                Console.WriteLine("Содержимое файла:");
+                Console.WriteLine(text);             
+            }
+        }
+    }
+}
