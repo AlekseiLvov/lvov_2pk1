0aa862a497b97f2a185f88f6b31bc081f582c5b2
AlekseyLvov <91681465+AlekseiLvov@users.noreply.github.com>
2022-11-27 21:19:20 +0300

    Add files via upload

---
+++ pz_13/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace pz_13
+{
+    internal class Program
+    {
+        static int met(float[] array, int n)
+        {
+            return array.Where(x => x > n).ToArray().Length;
+        }
+        static void Main(string[] args)
+        {
+            float[] arr = { 1.2f, 123, 324, 23, 1 };
+            Console.WriteLine(met(arr, 5));
+        }
+    }
+}
