5c65456b365754bc5e285b746e8ca85255111386
AlekseyLvov <91681465+AlekseiLvov@users.noreply.github.com>
2023-03-18 18:39:21 +0300

    Add files via upload

---
+++ pz_21/Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace pz_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ParkingTicket tickets = new ParkingTicket();
+            tickets.SetInfo();
+            tickets.getInfo();
+
+        }
+    }
+}
